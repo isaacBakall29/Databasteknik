7dc41c95c4865dca932fbb83222e3dd4ff98910e
isaacBakall29 <isaacbakall29@gmail.com>
2025-02-04 10:21:01 +0100

    added jdbc main and jdbc connector

---
+++ src/main/java/org/example/DatabaseConnection.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DatabaseConnection {
+
+	private static Connection connection;
+
+	static {
+		try {
+			Properties properties = new Properties();
+			properties.load(new FileInputStream("config.properties"));
+
+			String url = properties.getProperty("db.url");
+			String user = properties.getProperty("db.user");
+			String password = properties.getProperty("db.password");
+
+			connection = DriverManager.getConnection(url, user, password);
+			System.out.println("Database connected successfully!");
+
+		} catch (IOException | SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Failed to connect to database");
+		}
+	}
+
+	public static Connection getConnection() {
+		return connection;
+	}
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,16 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		try (Connection conn = DatabaseConnection.getConnection()) {
+			if (conn != null) {
+				System.out.println("Connection is active!");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 }
